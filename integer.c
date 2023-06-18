d8e3d8037509722e34b3a10184e8adabc1d905da
300406uchman <uchee0070@gmail.com>
2023-06-18 22:24:15 +0100

    integer.c

---
+++ integer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+*main-Entry point
+*
+*Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+int x = 5;
+int y = 10;
+
+if (x > y)
+{
+printf("the highest number is %d", x);
+}
+else if (y > x)
+{
+printf("the highest number is %d", y);
+}
+printf("\n");
+return (0);
+}
